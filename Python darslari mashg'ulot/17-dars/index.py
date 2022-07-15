202376338e853fc62fea72d214d4b3590f0d0201
saman4ik <nuriddinovmirjahon7@gmail.com>
2022-07-15 17:20:37 +0500

    ajaal

---
+++ Python darslari mashg'ulot/17-dars/index.py
@@ -0,0 +1,7 @@
+ism = input("Ismingiz nima? ")
+savol = f"Salom, {ism.title()}. Yoshingiz nechida? "
+yosh = input(savol)
+yosh = int(yosh) # yosh ni butun songa o'tkazamiz
+height = input("Bo'yingiz necha metr? ")
+height = float(height) # bo'yni o'nlik songa o'tkazamiz
+
